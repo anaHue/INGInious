2c4a65da204d63a9ed3fd85bef85f016b0389e2e
Anthony Gégo <anthonygego@hotmail.com>
2023-08-22 11:10:30 +0200

    [task_dispenser/toc] init config dict if empty

--- inginious/frontend/task_dispensers/toc.py
+++ inginious/frontend/task_dispensers/toc.py
@@ -23,7 +23,17 @@ class TableOfContents(TaskDispenser):
     def __init__(self, task_list_func, dispenser_data, database, course_id):
         # Check dispenser data structure
         dispenser_data = dispenser_data or {"toc": {}, "config": {}}
-        if not isinstance(dispenser_data, dict) or "toc" not in dispenser_data or "config" not in dispenser_data:
+
+        # Allow to support older structures
+        if isinstance(dispenser_data, list):
+            dispenser_data = {"toc": dispenser_data}
+
+        if not dispenser_data.get("config", {}):
+            dispenser_data["config"] = {
+                taskid: {item.get_id(): item.default for item in self.config_items} for taskid in task_list_func()
+            }
+
+        if not isinstance(dispenser_data, dict) or "toc" not in dispenser_data:
             raise Exception("Invalid dispenser data structure")
 
         TaskDispenser.__init__(self, task_list_func, dispenser_data, database, course_id)
